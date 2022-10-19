eb8812be3da0fab4eb27e0210c993a9feda51680
ranasajjadahmad5785 <99539588+ranasajjadahmad5785@users.noreply.github.com>
2022-10-19 14:22:48 +0500

    Personal Use

---
+++ ArrowsMover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowsMover : MonoBehaviour
+{
+    public GameObject[] Arrow;
+    public float delayTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Delay");
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(delayTime);
+        EnableAll();
+        yield return new WaitForSeconds(delayTime);
+        DisableAll();
+        StartCoroutine("Delay");
+    }
+
+    void EnableAll()
+    {
+        for (int i = 0; i < Arrow.Length; i++)
+        {
+            Arrow[i].SetActive(true);
+        }
+    }
+
+    void DisableAll()
+    {
+        for (int i = 0; i < Arrow.Length; i++)
+        {
+            Arrow[i].SetActive(false);
+        }
+    }
+}
